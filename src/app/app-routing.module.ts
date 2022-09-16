8e9170a51210a496e665c9e14acaa43bca4d106a
italoAl <italoalves20112010@hotmail.com>
2022-09-16 14:25:17 -0300

    Implemantar Rotas, RedirectTo, Rotas Ativas, Redirecionamento, Children e Lazy Loading.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '' , component: HomeComponent, pathMatch: 'full'},
+{path: 'sobre', component: SobreComponent, children: [
+  {path: 'italo', component: SobreComponent},
+]},
+{path: 'dashboard',
+ loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
+{path: '404', component: PageErrorComponent},
+{path: '**', redirectTo: '404'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { 'useHash': true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+// PAGES
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    PageErrorComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
